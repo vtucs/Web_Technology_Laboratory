5da148b0670c50bab25cabe9fb64da356ec0b43e
Shivam Nagpal <shivamnagpal432@gmail.com>
2019-11-21 15:42:07 +0530

    Add Simple Calculator PHP

---
+++ 08.01.Simple_Calculator.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+    <title>Simple Calculator</title>
+</head>
+
+<body>
+<form method="post">
+    <label>
+        Input 1:
+        <input type="text" name="num1"/>
+    </label> <br>
+    <label>
+        Input 2:
+        <input type="text" name="num2"/>
+    </label> <br>
+    <input type="submit" value="Calculate">
+
+</form>
+<?php
+if (isset($_POST['num1']) and isset($_POST['num2'])) {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    if (is_numeric($num1) and is_numeric($num1)) {
+        echo "<table>";
+        echo "<tr><td> Addition: </td><td>" . ($num1 + $num2) . "</td>";
+        echo "<tr><td> Subtraction: </td><td>" . ($num1 - $num2) . "</td>";
+        echo "<tr><td> Multiplication: </td><td>" . ($num1 * $num2) . "</td>";
+        echo "<tr><td>Division: </td><td>" . ($num1 / $num2) . "</td>";
+        echo "</table>";
+    } else {
+        echo "<script type='text/javascript' > alert('Invalid Numbers');</script>";
+    }
+}
+?>
+</body>
+</html>
