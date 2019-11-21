fd4054f76ad1f576078fc50125909b288166754d
Shivam Nagpal <shivamnagpal432@gmail.com>
2019-11-21 17:54:23 +0530

    Add question statement

--- 08.02.Matrices_Operations.php
+++ 08.02.Matrices_Operations.php
@@ -1,3 +1,11 @@
+<!--
+8. Write the PHP programs to do the following:
+a. Implement simple calculator operations.
+b. Find the transpose of a matrix.
+c. Multiplication of two matrices.
+d. Addition of two matrices.
+-->
+
 <!DOCTYPE html>
 <head>
     <title>Simple Calculator</title>
